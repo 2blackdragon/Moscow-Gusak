a6e54ea7eee6a909c13a22325449c624474164e6
Nastya <gusak-an@stu.1502.moscow>
2020-12-02 19:52:37 +0300

    Second commit

--- first.py
+++ first.py
@@ -1,6 +1,22 @@
-def main():
-    print('My first git-repo')
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("Ui.ui", self)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
